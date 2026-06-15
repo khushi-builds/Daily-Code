ea59a36c08d66e2bbdbdb8c093c809cc7b991116
Khushi Gupta <khushi307gupta@gmail.com>
2026-06-15 08:54:58 +0530

    Add Python variables and types fundamentals

---
+++ python/01-fundamentals/01_variables_types.py
@@ -0,0 +1,17 @@
+"""Variables, Types, I/O, Operators"""
+
+# Variables & Types
+x = 10
+y = 3.14
+name = "Khushi"
+is_active = True
+
+# Type conversion
+print(int("5"), float("5"), str(5), bool(0))
+
+# I/O
+n = int(input("Enter a number: "))
+print(f"You entered {n}")
+
+# Operators
+print(7 // 2, 7 % 2, 2 ** 3)
